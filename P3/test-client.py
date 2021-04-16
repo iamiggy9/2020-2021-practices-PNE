8212ebe76197a3c492a829a3778ad93cba39207b
iamiggy9 <iarrranz9@gmail.com>
2021-04-16 17:34:09 +0200

    Moved files to correct folders

--- P3/test-client.py
+++ P3/test-client.py
@@ -1,5 +1,6 @@
 
 from Client0 import Client
+list_sequences = ['ACTGGATAGCA','AACTCCCCCCCCCCCC','ACTGG', 'ATGGGGGCA', 'TTTGAAAAAGGTA']
 print('-----| Practice 3, Exercise 7 |------')
 IP = "127.0.0.1"
 PORT = 8080
@@ -13,10 +14,10 @@ if msg == 'GET':
     print(c.talk(msg + ' 3'))
     print(c.talk(msg + ' 4'))
 elif msg == 'INFO':
-    print(c.talk(msg + ' ACTGGATAGCA'))
+    print(c.talk(msg + ' ' + list_sequences[0]))
 elif msg == 'COMP':
-    print(c.talk(msg + ' ACTGGATAGCA'))
+    print(c.talk(msg + ' ' + list_sequences[0]))
 elif msg == 'REV':
-    print(c.talk(msg + ' ACTGGATAGCA'))
+    print(c.talk(msg + ' ' + list_sequences[0]))
 else:
     print(c.talk(msg))
