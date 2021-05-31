5d697af0a56c1b298da4b5c778a57e1983629f40
iamiggy9 <iarrranz9@gmail.com>
2021-05-31 18:28:55 +0200

    Moved files to correct folders

---
+++ Final-Project/Seq1.py
@@ -0,0 +1,85 @@
+import pathlib
+
+
+class Seq:
+    NULL='NULL'
+    INVALID='ERROR'
+
+    def __init__(self, strbases=NULL):
+        if strbases == Seq.NULL:
+            print("NULL Seq Created")
+            self.strbases = strbases
+        elif Seq.is_valid_sequence_2(strbases):
+            self.strbases=strbases
+            print('New sequence created')
+        else:
+            self.strbases=Seq.INVALID
+            print('Invalid sequence')
+
+
+
+    @staticmethod
+    def is_valid_sequence_2(strbases):
+        for c in strbases:
+            if c!='A' and c !='C' and c!='G' and c!= 'T':
+               return False
+        return True
+
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        if self.strbases == Seq.NULL or self.strbases==Seq.INVALID:
+            return 0
+        else:
+            return len(self.strbases)
+
+    def count_base(self, base):
+        return self.strbases.count(base)
+
+    def count(self):
+        bases = ["A", "C", "T", "G"]
+        count_bases = []
+        for base in bases:
+            count_bases.append(self.count_base(base))
+        dictionary = dict(zip(bases, count_bases))
+        return dictionary
+
+    def percentage(self):
+        list_bases = ["A", "C", "T", "G"]
+        list_percentages = []
+        total = len(self.strbases)
+        for base in list_bases:
+            percentage = self.count_base(base) * 100 / total
+            list_percentages.append(percentage)
+        return list_percentages
+
+    @staticmethod
+    def reverse(seq_rev):
+        rev_seq = ''
+        for e in seq_rev[::-1]:
+          rev_seq += e
+        return rev_seq
+    @staticmethod
+    def complement(seq_comp):
+        BLANK = ''
+        for e in seq_comp:
+            if e == "A":
+                BLANK += "T"
+            if e == "T":
+                BLANK += "A"
+            if e == "C":
+                BLANK += "G"
+            if e == "G":
+                BLANK += "C"
+        return BLANK
+
+    @staticmethod
+    def read_fasta(filename):
+            file_lines = pathlib.Path('../P3/' + str(filename) + '.txt').read_text().split("\n")
+            body = (file_lines[1:])
+            strbases = ''.join(body)
+            return strbases
+
+
+
--- Final-Project/server.py
+++ Final-Project/server.py
@@ -2,18 +2,28 @@ import http.server
 import socketserver
 import termcolor
 import colorama
+from Seq1 import Seq
 import server_utils as su
-import s2 as s_u
 from urllib.parse import urlparse, parse_qs
 import http.client
 import json
-
+DICT_GENES = {
+    'FRAT1': 'ENSG00000165879',
+    'ADA': 'ENSG00000196839',
+    'FXN': 'ENSG00000165060',
+    'RNU6_269P': 'ENSG00000212379',
+    'MIR633': 'ENSG00000207552',
+    'TTTY4C': 'ENSG00000228296',
+    'RBMY2YP': 'ENSG00000227633',
+    'FGFR3': 'ENSG00000068078',
+    'KDR': 'ENSG00000128052',
+    'ANK2': 'ENSG00000145362'
+}
 
 
 # Define the Server's port
 PORT =8080
 
-HTML_ASSETS = "./HTML/"
 SERVER = 'rest.ensembl.org'
 Parameters = "?content-type=application/json"
 
@@ -49,30 +59,97 @@ class TestHandler(http.server.BaseHTTPRequestHandler): # this class is inside th
         print("Resource requested: ", path_name)
         print("Parameters: ", arguments)
         context = {}
-        if path_name == "/":
-            contents = su.read_template_html_file("./html/INDEX.html").render(context=context)
-        elif path_name == "/test": # when working with forms my self.path always comes with a question mark at the end
-            contents = su.read_template_html_file("Ctest.html").render()
-        elif path_name =='/listSpecies':
-            end = '/html/species'
-            contents = s_u.list_seqs(connection, end, Parameters, arguments, context)
-
-        elif path_name == '/operation':
-            sequence = arguments['sequence'][0]
-            operation = arguments['operation'][0]
-            if operation=='Info':
-                contents=su.info(sequence)
-            elif operation=='Rev':
-                contents = su.rev(sequence)
-            elif operation == 'Comp':
-                contents = su.comp(sequence)
-
-        elif path_name =='/gene':
-            gene = arguments['gene'][0]
-            contents = su.gene(gene)
-        else:
+        try:
+            if path_name == "/":
+                contents = su.read_template_html_file("./html/INDEX.html").render(context=context)
+            elif path_name == "/listSpecies":
+                ENDPOINT = "/info/species"
+                connection.request("GET", ENDPOINT + Parameters)
+                response = connection.getresponse()
+                response_dict = json.loads(response.read().decode())
+                species_list = []
+                amount_species = len(response_dict["species"])
+                context["amount_species"] = amount_species
+                limit = int(arguments["limit"][0])
+                context["limit"] = limit
+                for n in range(0, limit):
+                    species_list.append(response_dict["species"][n]["common_name"])
+                context["names"] = species_list
+                contents = su.read_template_html_file("html/LISTSPECIES.html").render(context=context)
+            elif path_name == "/karyotype":
+                ENDPOINT = "info/assembly/"
+                specie = arguments["species"][0]
+                connection.request("GET", ENDPOINT + specie + Parameters)
+                response = connection.getresponse()
+                response_dict = json.loads(response.read().decode())
+                karyotype = response_dict["karyotype"]
+                context["species"] = arguments["species"][0]
+                context["karyotype"] = karyotype
+                contents = su.read_template_html_file("./html/KARYOTYPE.html").render(context=context)
+            elif path_name == "/chromosomeLength":
+                ENDPOINT = "info/assembly/"
+                specie = arguments["species"][0]
+                connection.request("GET", ENDPOINT + specie + Parameters)
+                response = connection.getresponse()
+                response_dict = json.loads(response.read().decode())
+                chromosome = arguments["chromosome"][0]
+                print(response_dict['top_level_region'])
+                for n in range(0, len(response_dict["top_level_region"])):
+                    if chromosome == response_dict["top_level_region"][n]["name"]:
+                        length = response_dict["top_level_region"][n]["length"]
+                context["length"] = length
+                contents = su.read_template_html_file("./html/CHROMOSOMELENGTH.html").render(context=context)
+            elif path_name == "/geneSeq":
+                ENDPOINT = "/sequence/id/"
+                gene = arguments["gene"][0]
+                id = DICT_GENES[gene]
+                connection.request("GET", ENDPOINT + id + Parameters)
+                response = connection.getresponse()
+                response_dict = json.loads(response.read().decode())
+                context["seq"] = response_dict["seq"]
+                contents = su.read_template_html_file("./html/SEQGENE.html").render(context=context)
+            elif path_name == "/geneInfo":
+                ENDPOINT = "/sequence/id/"
+                gene = arguments["gene"][0]
+                id = DICT_GENES[gene]
+                connection.request("GET", ENDPOINT + id + Parameters)
+                response = connection.getresponse()
+                response_dict = json.loads(response.read().decode())
+                # print(json.dumps(response_dict, indent=4, sort_keys=True))
+                info = response_dict["desc"].split(":")
+                context["dict_info"] = {
+                    "Name": info[1],
+                    "ID": id,
+                    "Start": info[3],
+                    "End": info[4],
+                    "Length": (int(info[4]) - int(info[3]) + 1)
+                }
+                contents = su.read_template_html_file("./html/INFOGENE.html").render(context=context)
+            elif path_name == "/geneCalc":
+                ENDPOINT = "/sequence/id/"
+                gene = arguments["gene"][0]
+                id = DICT_GENES[gene]
+                connection.request("GET", ENDPOINT + id + Parameters)
+                response = connection.getresponse()
+                response_dict = json.loads(response.read().decode())
+                # print(json.dumps(response_dict, indent=4, sort_keys=True))
+                sequence = Seq(response_dict["seq"])
+                dict_bases = Seq.count(sequence)
+                percentage = Seq.percentage(sequence)
+                context["length"] = Seq.len(sequence)
+                context["bases"] = {
+                    "A": str(dict_bases["A"]) + " (" + str(percentage[0]) + "%)",
+                    "C": str(dict_bases["C"]) + " (" + str(percentage[1]) + "%)",
+                    "T": str(dict_bases["T"]) + " (" + str(percentage[2]) + "%)",
+                    "G": str(dict_bases["G"]) + " (" + str(percentage[3]) + "%)"
+                }
+                contents = su.read_template_html_file("./html/CALCGENE.html").render(context=context)
+            else:
+                contents = su.read_template_html_file("./html/ERROR.html").render()
+        except KeyError:
             contents = su.read_template_html_file("./html/ERROR.html").render()
-
+        except IndexError:
+            contents = su.read_template_html_file("./html/ERROR.html").render()
         # Generating the response message
         self.send_response(200)  # -- Status line: OK!
 
